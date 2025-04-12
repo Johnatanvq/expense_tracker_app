7536111f5d528ee91c94e3b571c73f6d629a3517
Johnatan Valencia <johnatanv91@gmail.com>
2025-04-12 17:30:39 -0500

    new expense component added

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/expenses_list/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
@@ -30,7 +31,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       context: context, 
-      builder: (ctx) => Text('data')
+      builder: (ctx) => const NewExpense(),
     );
   }
 
