f9ff3aea00b559d5ffd56f5c97d6244b1d4bd4cd
johnatan <johnatanv91@gmail.com>
2025-08-18 16:08:27 -0500

    now new expenses can be created and saved

--- lib/widgets/expenses_list/new_expense.dart
+++ lib/widgets/expenses_list/new_expense.dart
@@ -3,7 +3,12 @@ import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:expense_tracker/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({
+    super.key,
+    required this.onAddExpense,
+  });
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<StatefulWidget> createState(){
@@ -60,6 +65,13 @@ class _NewExpenseState extends State<NewExpense>{
       );
       return;
     }
+    widget.onAddExpense(Expense(
+        title: _titleController.text, 
+        amount: enteredAmount, 
+        date: _selectedDate!, 
+        category: _selectedCategory
+      ),
+    );
   }
   
   // void saveTitleInput(String inputValue) {
