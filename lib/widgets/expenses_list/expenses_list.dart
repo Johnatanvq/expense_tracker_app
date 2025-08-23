031e6d5be25a3b760a5ed1b6995883e0342d0049
johnatan <johnatanv91@gmail.com>
2025-08-23 17:44:52 -0500

    using the dismissible widget for remove list expense items

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -6,16 +6,25 @@ class ExpensesList extends StatelessWidget {
   const ExpensesList({
     super.key,
     required this.expenses,
+    required this.onRemovedExpense,
   });
 
   //ListView.builder arroja la lista de elementos únicamente visibles
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemovedExpense;
   @override
   Widget build(context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) =>
-        ExpenseItem(expenses[index])
+      itemBuilder: (ctx, index) => Dismissible(
+          key: ValueKey(key), 
+          onDismissed: (direction) {
+            onRemovedExpense(expenses[index]);
+          },
+          child: ExpenseItem(
+            expenses[index],
+          )
+        ),
     );
   }
 }
