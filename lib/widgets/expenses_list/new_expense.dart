92a485b29b79fefaca6c4cd631b1c5394ec517d0
johnatan <johnatanv91@gmail.com>
2025-08-23 17:33:41 -0500

    creating full screen modal to new expense

--- lib/widgets/expenses_list/new_expense.dart
+++ lib/widgets/expenses_list/new_expense.dart
@@ -72,6 +72,7 @@ class _NewExpenseState extends State<NewExpense>{
         category: _selectedCategory
       ),
     );
+    Navigator.pop(context);
   }
   
   // void saveTitleInput(String inputValue) {
@@ -88,7 +89,7 @@ class _NewExpenseState extends State<NewExpense>{
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(16),
+      padding: EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(
         children: [
           TextField(
