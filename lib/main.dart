daa8b4abb68e561a0c077c1515d839da8bce2b77
Johnatan <johnatanv91@gmail.com>
2025-08-30 10:13:51 -0500

    create custom theme on Materialapp property

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3:  true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: 
+
+      ),
       home: Expenses(),
     ),
   );
