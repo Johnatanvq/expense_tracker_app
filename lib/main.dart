4eeeef8382eb085b6d8b066435101557b3bf45ae
Johnatan <johnatanv91@gmail.com>
2025-09-05 19:44:22 -0500

    dark theme added, shows depending on system

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,31 @@ import 'package:expense_tracker/widgets/expenses.dart';
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
+
+var kdarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 9, 125)
+);
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kdarkColorScheme,
+        cardTheme: const CardThemeData().copyWith(
+          color: kdarkColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(
+            horizontal: 16, 
+            vertical: 8
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kdarkColorScheme.primaryContainer,
+            foregroundColor: kdarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: kColorScheme,
@@ -36,6 +58,7 @@ void main() {
         ),
 
       ),
+      themeMode: ThemeMode.system,
       home: Expenses(),
     ),
   );
